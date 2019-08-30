c59267d63f5ba667589a1a37b95bb4b56dd0a2da
Ulises <uli.r19@gmail.com>
2019-08-30 18:46:58 -0300

    Se saca la parte de copiar el proyecto desde el contenedor al host.

---
+++ 5/debian-9-exe/rootfs/app-entrypoint.sh
@@ -0,0 +1,91 @@
+#!/bin/bash -e
+. /opt/bitnami/base/functions
+
+print_welcome_page
+
+INIT_SEM=/tmp/initialized.sem
+
+fresh_container() {
+  [ ! -f $INIT_SEM ]
+}
+
+app_present() {
+  [ -f /app/config/database.php ]
+}
+
+vendor_present() {
+  [ -f /app/vendor ]
+}
+
+composer_json_present() {
+  [ -f /app/composer.json ]
+}
+
+
+wait_for_db() {
+  local db_host="${DB_HOST:-mariadb}"
+  local db_port="${DB_PORT:-3306}"
+  local db_address=$(getent hosts "$db_host" | awk '{ print $1 }')
+  counter=0
+  log "Conectando a mariadb en $db_address"
+  while ! curl --silent "$db_address:$db_port" >/dev/null; do
+    counter=$((counter+1))
+    if [ $counter == 30 ]; then
+      log "Error: No se pudo conectar a mariadb."
+      exit 1
+    fi
+    log "Intentando conectar a mariadb en $db_address. Intento nro. $counter."
+    sleep 5
+  done
+}
+
+setup_db() {
+  log "Configurando la base de datos"
+  sed -i "s/utf8mb4/utf8/g" /app/config/database.php
+  php artisan migrate --force
+}
+
+if [ "${1}" == "php" -a "$2" == "artisan" -a "$3" == "serve" ]; then
+  if ! app_present; then
+    log "No se puede iniciar el contenedor con una aplicación Laravel desde cero."
+    log "Debe iniciarla desde un proyecto que ya existente."
+    exit 1
+  fi
+
+  if composer_json_present ; then
+    log "Instalando/Actualizando las dependencias de Laravel (composer)"
+    if ! vendor_present; then
+      composer install
+      log "Dependencias Instaladas"
+    else
+      composer update
+      log "Dependencias actualizadas"
+    fi
+  else
+    log "No cuenta con un archivo composer.json"
+    log "Esto puede significar que:"
+    log "- El proyecto no tiene dependencias"
+    log "- No tiene clonado el repositorio"
+    exit 1
+  fi
+
+  wait_for_db
+
+  if ! fresh_container; then
+    echo "#########################################################################"
+    echo "                                                                         "
+    echo " La inicialización de la app se saltea:                                  "
+    echo " Elimine el archivo $INIT_SEM y reinicie el contenedor para reinicializar"
+    echo " Usted puede, alternativamente, correr cualquier comando específico      "
+    echo " utilizando docker-compose exec:                                         "
+    echo " ej. docker-compose exec myapp php artisan make:console FooCommand       "
+    echo "                                                                         "
+    echo "#########################################################################"
+  else
+    setup_db
+    log "La inicialización finalizó"
+    touch $INIT_SEM
+  fi
+fi
+
+exec tini -- "$@"
